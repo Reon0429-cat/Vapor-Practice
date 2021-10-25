675f4af68cd47704577f5fbc770b3866026fe29e
Reon0429-cat <oonishireon110@gmail.com>
2021-10-26 08:39:06 +0900

    routes練習

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,34 @@
 import Vapor
+import Leaf
+import LeafKit
 
+// アクセスするURLを設定することができる
 func routes(_ app: Application) throws {
-    app.get { req in
-        return req.view.render("index", ["title": "Hello Vapor!"])
+    
+    // Viewsのindex.leafファイルをレンダリング
+    // index.leafの#titleにHello Vapor!を入れている
+    app.get("KKK") { request in
+        return request.view.render("like", ["title": "Hello"])
     }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    
+    // http://localhost:8080/AAA/BBB でアクセスするとCCCを返す
+    // 「/」で区切るのはだめ
+    app.get("AAA", "BBB") { request in
+        return "CCC"
     }
+    
+    // URLの省略
+    // http://localhost:8080/X/Y/Z でアクセスできる
+    let appGrouped = app.grouped("ABC", "DEF")
+    appGrouped.get("Z") { request in
+        return "REON"
+    }
+    
+    // :id, :nameなどとすることで引数を渡せる
+    app.get("User", ":id", ":name") { request -> String in
+        let id = request.parameters.get("id")
+        let name = request.parameters.get("name")
+        return "Hello, world!" + id! + name!
+    }
+    
 }
